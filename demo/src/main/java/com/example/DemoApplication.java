fc57ca0ffe39bac8ec5cbf96e1d9dc35b473d526
pete <pjjuliana216@gmail.com>
2016-02-10 19:50:13 -0500

    Added a comment to the DemoApplication class

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -3,9 +3,17 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * Main class for the demo app. 
+ * This comment is to test some git shit.
+ * @author pete
+ *
+ */
 @SpringBootApplication
 public class DemoApplication {
-
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
